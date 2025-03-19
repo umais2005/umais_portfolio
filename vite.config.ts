227024b7740018cb6a08df81d8ed1868d7e914aa
Umais Siddiqui <umaismuhammad99@gmail.com>
2025-03-19 17:16:07 +0500

    gh suitable

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,11 +6,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/umais_portfolio/',
   server: {
     host: "::",
     port: 8080,
   },
+  base: "/umais_portfolio/", // Set base path for GitHub Pages
   plugins: [
     react(),
     mode === 'development' &&
